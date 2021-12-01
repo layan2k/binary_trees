bad8ffb4cea7e0258126934ff41e1d9eaceb1a6d
layan2k <lesliedeeshumba@gmail.com>
2021-12-01 23:54:02 +0200

    betty

--- binary_trees.h
+++ binary_trees.h
@@ -45,6 +45,12 @@ int binary_tree_is_parent(const binary_tree_t *node);
 int binary_tree_balance(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t recursive_height(const binary_tree_t *tree);
-
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_parent_full(const binary_tree_t *node);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif
