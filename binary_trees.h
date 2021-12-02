ce7e5fdb88bcde6d63f2caada25ca893aed3e7e9
layan2k <lesliedeeshumba@gmail.com>
2021-12-02 02:02:22 +0200

    fix

--- binary_trees.h
+++ binary_trees.h
@@ -55,7 +55,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 size_t recursive_depth(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-				     const binary_tree_t *second)
+				     const binary_tree_t *second);
 size_t recursive_height(const binary_tree_t *tree);
 void print_level(const binary_tree_t *tree, int level, void (*func)(int));
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
